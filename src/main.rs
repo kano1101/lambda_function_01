2c293a7bfc840b6e3bcfe8a86765fe7e4789bbee
Akira Kano <a.kano1101@gmail.com>
2022-10-31 12:24:06 +0900

    transactionを多重にしたい場合、tx.beginでOKなのがわかった

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use lambda_http::{run, service_fn, Body, Request, Response};
 use serde::{Deserialize, Serialize};
 use serde_with::serde_as;
 
+use sqlx::Acquire as _;
+
 #[tokio::test]
 async fn test_system_get() -> anyhow::Result<()> {
     let test: String = reqwest::get("http://localhost:9000/lambda-url/lambda_function_01")
@@ -24,31 +26,29 @@ async fn test_system_get() -> anyhow::Result<()> {
 }
 #[tokio::test]
 async fn test_unit_func() -> anyhow::Result<()> {
-    async fn db_setup(transaction: &mut sqlx::Transaction<'_, sqlx::MySql>) -> anyhow::Result<()> {
-        // let command = r#"CREATE TABLE IF NOT EXISTS users (id int, name varchar(64));"#;
-        // sqlx::query(command).execute(transaction).await?;
-
-        // let (id, name) = (42, "Fumiya");
-        // let command = r#"INSERT INTO users VALUES (?, ?);"#;
-        // sqlx::query(command)
-        //     .bind(id)
-        //     .bind(name)
-        //     .execute(transaction)
-        //     .await?;
+    async fn db_setup(tx: &mut sqlx::Transaction<'_, sqlx::MySql>) -> anyhow::Result<()> {
+        let (id, name) = (42, "Fumiya");
+        let command = r#"INSERT INTO users VALUES (?, ?);"#;
+        sqlx::query(command).bind(id).bind(name).execute(tx).await?;
         Ok(())
     }
-    async fn db_teardown() {
-        // tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    async fn db_teardown(tx: &mut sqlx::Transaction<'_, sqlx::MySql>) -> anyhow::Result<()> {
+        let command = r#"DELETE FROM users WHERE name="Fumiya";"#;
+        sqlx::query(command).execute(tx).await?;
+        Ok(())
     }
 
-    let request = Request::default();
+    let event = Request::default();
 
     let pool = establish_connection_or_get_cache().await;
-    let mut transaction = pool.begin().await?;
-    let succeed_response =
-        lambda_dev::run_test_async(db_setup(&mut transaction), func(request), db_teardown())
-            .await?;
-    transaction.commit().await?;
+    let mut tx = pool.begin().await?;
+    let mut tx = tx.begin().await?;
+    db_setup(&mut tx).await?;
+    let mut tx = tx.begin().await?;
+    let succeed_response = func(&mut tx, event).await?;
+    let mut tx = tx.begin().await?;
+    db_teardown(&mut tx).await?;
+    tx.commit().await?;
 
     let v: Vec<SqlxArticle> = match succeed_response.into_body() {
         Body::Text(t) => serde_json::from_str(&t)?,
@@ -57,10 +57,16 @@ async fn test_unit_func() -> anyhow::Result<()> {
 
     assert_eq!(
         v,
-        vec![SqlxArticle {
-            id: 1,
-            name: "Saki".to_string(),
-        }]
+        vec![
+            SqlxArticle {
+                id: 1,
+                name: "Saki".to_string(),
+            },
+            SqlxArticle {
+                id: 42,
+                name: "Fumiya".to_string(),
+            }
+        ]
     );
 
     Ok(())
@@ -77,21 +83,25 @@ struct SqlxArticle {
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
 
-    run(service_fn(func)).await.map_err(|_| core::fmt::Error)?;
+    let pool = establish_connection_or_get_cache().await;
+
+    run(service_fn(|event| async move {
+        let mut tx = pool.begin().await.unwrap();
+        func(&mut tx, event).await
+    }))
+    .await
+    .map_err(|_| core::fmt::Error)?;
+
     Ok(())
 }
 
-async fn func(event: Request) -> anyhow::Result<Response<Body>> {
-    let pool = establish_connection_or_get_cache().await;
-
+async fn func<'a>(
+    tx: &mut sqlx::Transaction<'a, sqlx::MySql>,
+    _event: Request,
+) -> anyhow::Result<Response<Body>> {
     let query = r#"SELECT * FROM users;"#;
-    async fn select(pool: &sqlx::MySqlPool, query: &str) -> anyhow::Result<Vec<SqlxArticle>> {
-        let users = sqlx::query_as::<_, SqlxArticle>(query)
-            .fetch_all(pool)
-            .await?;
-        Ok(users)
-    }
-    let users: Vec<SqlxArticle> = bench("select all users", select(pool, query)).await?;
+    let users = sqlx::query_as::<_, SqlxArticle>(query).fetch_all(tx);
+    let users: Vec<SqlxArticle> = bench("select all users", users).await?;
 
     let resp = Response::builder()
         .status(200)
